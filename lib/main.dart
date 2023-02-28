0454178757af0f714df80b3cb24a980b86a7d847
joseph-106 <sjjoseph106@gmail.com>
2023-02-28 14:25:55 +0900

    Sign Up Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tictoc/constants/sizes.dart';
 import 'package:tictoc/features/authentication/sign_up_screen.dart';
 
 void main() {
@@ -13,7 +14,19 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TicToc',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xFFE9435A),
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
       home: const SignUpScreen(),
     );
